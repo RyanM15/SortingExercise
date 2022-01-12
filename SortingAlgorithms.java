659abcb19095bac4545fb5efbd1c41cdad7a1bdd
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2022-01-12 08:53:25 -0500

    reset the array so that when printing all of the different sorts it prints correctly

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,13 +1,18 @@
 public class SortingAlgorithms{
-	//variables used throughout the program in all three methods
-	private int[] arr = {1, 2, 3};
-	private int swaps = 0;
-	private int temp = 0;
-	private int min = 0;
 	
-	//Bubble Sort. Takes an array and compares the first two numbers, finds the larger value and places it on the right. This is repeated throuhgout the array until the 
-	//largest number is to the far right. It then repeats the process until the array has been sorted in increasing order.
-	public void bubbleSort(){  
+	private int[] array = {95,85,89,69,76,84,86,65,52,86,46,65,45,59,76,66,22,1,91,86,14,6,68,31,57,50,90,80,8,87,42,76,48,35,91,73,50,9,56,8,76,17,64,3,30,99,5,26,4,24,44,92,63,84,48,58,56,33,13,31,10,90,13,48,24,93,64,60,49,84,76,69,48,12,81,61,58,9,19,34,61,34,58,89,4,78,55,58,30,58,73,36,11,55,14,60,63,48,53,41};
+	
+	
+	
+	public void bubbleSort(){ 
+		int[] arr = new int[array.length];
+		for (int i = 0; i < array.length; i++){
+			arr[i] = array[i];
+		}
+	
+	int swaps = 0;
+	int temp = 0;
+	
 	
 	
 	int counter = 10;
@@ -35,10 +40,18 @@ public class SortingAlgorithms{
 	 System.out.print(swaps);
 }
 
-	//Sellection Sort. Sets the first number in an array to the minimum value, then goes through the rest of the array and compares that value to every other value and if 
-	//another number is smaller then it sets that number to the minimum. Once the program reaches the end of the array the minimum is set to the left of the array. This 
-	//process is repeated until the array is sorted in increasing order.
+
 	public void selectionSort(){ 
+		
+		int[] arr = new int[array.length];
+		for (int i = 0; i < array.length; i++){
+			arr[i] = array[i];
+		}
+	
+		
+		int swaps = 0;
+		int temp = 0;
+		int min = 0;
 		
 		for (int x = 0; x < arr.length - 1; x++){
 			
@@ -54,6 +67,7 @@ public class SortingAlgorithms{
 				arr[x] = arr[min];
 				arr[min] = temp;
 				swaps++;
+				//System.out.print(swaps);
 			}
 		}
 		System.out.print(swaps);
@@ -65,10 +79,19 @@ public class SortingAlgorithms{
 
 
 }
-	//Insertion Sort. Assumes the first number in the array is sorted. Then it goes to the next number, and if that number is less that the already sorted number, it moves 
-	//that number to the left of the already sorted number. If that number is larger, then it sees it as already sorted and continues. This process is repeated until the 
-	//array is sorted in increasing order. 
+
 	public void insertionSort(){ 
+		
+		int[] arr = new int[array.length];
+		for (int i = 0; i < array.length; i++){
+			arr[i] = array[i];
+		}
+	
+		
+		int swaps = 0;
+		int temp = 0;
+		int min = 0;
+		
 		for(int x = 1; x < arr.length; x++){
 			
 			
@@ -77,8 +100,9 @@ public class SortingAlgorithms{
 			while(y>0 && arr[y-1]>temp){
 				arr[y] = arr[y-1];
 				y--;
-				swaps++;
+				
 			}
+			swaps++;
 			arr[y] = temp;
 		}
 		System.out.println(swaps);
@@ -101,6 +125,11 @@ public class SortingAlgorithms{
 
 
 	
+		
+
+
+
+
 		
 
 
